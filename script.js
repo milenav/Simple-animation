657e2d5d444caeb935794090e93e4257170fd1ec
Milena Valcheva <milena_77@mail.bg>
2019-05-06 17:43:12 +0300

    change file

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    let content = $('.content');
+
+    $('#trigger').live('click', function() {
+        $(this).toggle(function() {
+            $(this).text('hide follow!');
+            content.removeClass('reverse').addClass('running');
+        }, function() {
+            $(this).text('follow me!');
+            content.removeClass('running').addClass('reverse');
+        }).trigger('click');
+    });
+
+});
+
+
+
+
